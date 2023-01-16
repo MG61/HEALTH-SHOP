92df19f1da3a2963d969ed3fe5c99c25552c8902
Никита Абраменко <abramenko098@gmail.com>
2023-01-16 21:33:21 +0300

    Создание клика в DataGrid

---
+++ Kurs7PM/Recipe.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kurs7PM
+{
+    /// <summary>
+    /// Логика взаимодействия для Recipe.xaml
+    /// </summary>
+    public partial class Recipe : Window
+    {
+        public Recipe()
+        {
+            InitializeComponent();
+        }
+
+        private void DOB_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DELETE_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UPDATE_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EXIT_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kurs7PM/Sklad.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kurs7PM
+{
+    /// <summary>
+    /// Логика взаимодействия для Sklad.xaml
+    /// </summary>
+    public partial class Sklad : Window
+    {
+        public Sklad()
+        {
+            InitializeComponent();
+        }
+
+        private void DOB_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DELETE_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UPDATE_sotr_Login(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EXIT_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
